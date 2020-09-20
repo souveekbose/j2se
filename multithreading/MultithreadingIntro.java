bd6b0c72892a306fe7cb400757ed3b94503df204
Souveek Bose <souveek.bose@sapient.com>
2020-09-20 19:38:46 +0530

    thread intro

---
+++ multithreading/MultithreadingIntro.java
@@ -0,0 +1,24 @@
+/**
+ * Thread creation: Implements Runnable
+ */
+public class MultithreadingIntro {
+    public static void main(String[] args) {
+        System.out.println("Name of main thread: "+Thread.currentThread().getName());
+
+        Waiter newWaiter = new Waiter();
+        Thread newWaiterThread = new Thread(newWaiter, "First-Waiter-Thread");
+        Thread newWaiterThread2 = new Thread(newWaiter);
+
+        newWaiterThread.start();
+        newWaiterThread2.start();
+    }
+}
+
+
+class Waiter implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Name of Waiter thread: "+Thread.currentThread().getName());
+    }
+}
